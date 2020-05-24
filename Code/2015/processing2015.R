3ef7991ebd3ee3d7872cbe59e295c2555e417bc6
Kevin Wang <kevin.wang@sydney.edu.au>
2020-05-24 13:07:24 +1000

    resolving 2015 + 2018 school funding variable error, ref #7, versioned school data

--- Code/2015/processing2015.R
+++ Code/2015/processing2015.R
@@ -4,14 +4,13 @@ library(here)
 sch_qqq <- read_sav(here("/Data/Raw/2015/PUF_SPSS_COMBINED_CMB_SCH_QQQ.zip"),
                     col_select = c(CNT,
                                    CNTSCHID,
-                                   SC016Q01TA,
-                                   SC016Q02TA,
-                                   SC016Q03TA,
-                                   SC002Q01TA,
-                                   SC002Q02TA,
-                                   SC002Q02TA,
+                                   SC016Q01TA, ## funding govt
+                                   SC016Q02TA, ## funding student fees
+                                   SC016Q03TA, ## funding benefactors
+                                   SC002Q01TA, ## num boys
+                                   SC002Q02TA, ## num girls
                                    STRATIO,
-                                   SC013Q01TA,
+                                   SC013Q01TA, ## public of private
                                    STAFFSHORT,
                                    W_SCHGRNRABWT,
                                    SCHSIZE))
