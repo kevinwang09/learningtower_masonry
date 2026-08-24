71b5450874ca8fbfd8b6482a1e5c3cdeeb3a62a1
Kevin Wang <kevinwang09@users.noreply.github.com>
2026-08-23 17:12:53 -0700

    fixed 2022 school and student missing values, ref #13

--- Code/process_pisa.R
+++ Code/process_pisa.R
@@ -466,6 +466,19 @@ book_levels_6 = function(x){
     TRUE ~ NA_character_) %>% as.factor()
 }
 
+book_levels_7 = function(x){
+  x = as.integer(x)
+  dplyr::case_when(
+    x == 1 ~ "0",
+    x == 2 ~ "1-10",
+    x == 3 ~ "11-25",
+    x == 4 ~ "26-100",
+    x == 5 ~ "101-200",
+    x == 6 ~ "201-500",
+    x == 7 ~ "More than 500",
+    TRUE ~ NA_character_) %>% as.factor()
+}
+
 public_private = function(x){
   x = as.integer(x)
   dplyr::case_when(
@@ -487,7 +500,8 @@ transformation_registry <- list(
   "yes1no2" = function(x) { yes1no2(x) },
   "none1one2two3threemore4" = function(x) { none1one2two3threemore4(x) },
   "public_private" = function(x) { public_private(x) },
-  "book_levels_6" = function(x) { book_levels_6(x) }
+  "book_levels_6" = function(x) { book_levels_6(x) },
+  "book_levels_7" = function(x) { book_levels_7(x) }
 )
 
 #' Apply Transformations and Rename to Target
