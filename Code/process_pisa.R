66114075a9d446a50f4e711bb471e2f33b34c335
Kevin Wang <kevinwang09@users.noreply.github.com>
2026-08-23 17:36:29 -0700

    refactor: improve NA handling logic in data processing and update environment metadata files

--- Code/process_pisa.R
+++ Code/process_pisa.R
@@ -556,12 +556,19 @@ transform_pisa_variables <- function(target_year, df, mapping_csv_path) {
     if (!is.na(na_str) && nchar(as.character(na_str)) > 0) {
       na_arr <- trimws(unlist(strsplit(as.character(na_str), ";")))
       for (na_code in na_arr) {
-        if (is.numeric(out_cols[[target]]) && !is.na(suppressWarnings(as.numeric(na_code)))) {
-          replacement <- as.numeric(na_code)
+        if (is.factor(out_cols[[target]])) {
+          if (na_code %in% levels(out_cols[[target]])) {
+            out_cols[[target]][out_cols[[target]] == na_code] <- NA
+          }
+        } else if (is.numeric(out_cols[[target]])) {
+          if (!is.na(suppressWarnings(as.numeric(na_code)))) {
+            replacement <- as.numeric(na_code)
+            out_cols[[target]] <- dplyr::na_if(out_cols[[target]], replacement)
+          }
         } else {
           replacement <- na_code
+          out_cols[[target]] <- dplyr::na_if(out_cols[[target]], replacement)
         }
-        out_cols[[target]] <- dplyr::na_if(out_cols[[target]], replacement)
       }
     }
     
